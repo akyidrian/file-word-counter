cf9a6a5bcf59f05d1dbb086e649e9ff01c0cb6fe
Akyidrian <akyidrian@gmail.com>
2018-05-16 09:20:26 -0700

    Added extra tests and tidied up some code

--- fileWordCounter.hpp
+++ fileWordCounter.hpp
@@ -13,21 +13,22 @@ typedef std::unordered_map<std::string, FWCList::iterator> FWCMap;
 
 class FileWordCounter {
 public:
-    FileWordCounter(const std::string file);
+    FileWordCounter(const std::string& file);
     ~FileWordCounter(void);
-    unsigned int count(std::string word);
+    unsigned int count(const std::string& word);
     unsigned int totalWordsCounted(void);
     std::vector<FWCPair> topNWords(const unsigned int n);
     void printTopNWords(const unsigned int n);
+    std::string getFilePath(void);
 
 private:
     FWCList words; // sorted by count in descending order after object construction
     FWCMap nodeMap;  // acts as a fact lookup to nodes in 'words' list
     std::string file;  // file path
 
-    std::string cleanWord(const std::string word);
-    std::queue<std::string> splitWordToken(const std::string token);
-    void addWord(const std::string word);
+    std::string cleanWord(const std::string& word);
+    std::queue<std::string> splitWordToken(const std::string& token);
+    void addWord(const std::string& word);
 };
 
 #endif /* FILE_WORD_COUNTER_H */
--- testFileWordCounter.cpp
+++ testFileWordCounter.cpp
@@ -74,6 +74,20 @@ TEST_CASE("Using FileWordCounter to count words from \"testFile.txt\"", "[count]
 TEST_CASE("Using FileWordCounter on \"testFile.txt\" to get the top N most used words", "[rank]") {
     FileWordCounter fwc("testFile.txt");
 
+    SECTION("Checking the top 5 most used words (in decending order)") {
+        std::vector<FWCPair> top5 = fwc.topNWords(5);
+        CHECK_FALSE(top5.empty());
+        CHECK(top5.size() == 6);
+
+        std::vector<FWCPair>::iterator it = top5.begin();
+        CHECK(it->first == "the"); CHECK(it->second == 19); it++;    // 1
+        CHECK(it->first == "brown"); CHECK(it->second == 17); it++;  // 2
+        CHECK(it->first == "quick"); CHECK(it->second == 17); it++;  // 2
+        CHECK(it->first == "fox"); CHECK(it->second == 14); it++;    // 3
+        CHECK(it->first == "jumped"); CHECK(it->second == 13); it++; // 4
+        CHECK(it->first == "over"); CHECK(it->second == 11); it++;   // 5
+    }
+
     SECTION("Checking the top 10 most used words (in decending order)") {
         std::vector<FWCPair> top10 = fwc.topNWords(10);
 
@@ -95,8 +109,53 @@ TEST_CASE("Using FileWordCounter on \"testFile.txt\" to get the top N most used 
         CHECK(it->first == "long"); CHECK(it->second == 6); it++;    // 9
         CHECK(it->first == "hello"); CHECK(it->second == 5); it++;   // 10
     }
+
+    SECTION("Checking the top 15 most used words (in decending order)") {
+        std::vector<FWCPair> top15 = fwc.topNWords(15);
+
+        CHECK_FALSE(top15.empty());
+        CHECK(top15.size() == 29);
+
+        std::vector<FWCPair>::iterator it = top15.begin();
+        CHECK(it->first == "the"); CHECK(it->second == 19); it++;          // 1
+        CHECK(it->first == "brown"); CHECK(it->second == 17); it++;        // 2
+        CHECK(it->first == "quick"); CHECK(it->second == 17); it++;        // 2
+        CHECK(it->first == "fox"); CHECK(it->second == 14); it++;          // 3
+        CHECK(it->first == "jumped"); CHECK(it->second == 13); it++;       // 4
+        CHECK(it->first == "over"); CHECK(it->second == 11); it++;         // 5
+        CHECK(it->first == "lazy"); CHECK(it->second == 10); it++;         // 6
+        CHECK(it->first == "dog"); CHECK(it->second == 9); it++;           // 6
+        CHECK(it->first == "double"); CHECK(it->second == 7); it++;        // 8
+        CHECK(it->first == "float"); CHECK(it->second == 7); it++;         // 8
+        CHECK(it->first == "integer"); CHECK(it->second == 6); it++;       // 9
+        CHECK(it->first == "long"); CHECK(it->second == 6); it++;          // 9
+        CHECK(it->first == "hello"); CHECK(it->second == 5); it++;         // 10
+        CHECK(it->first == "abcdefg"); CHECK(it->second == 2); it++;       // 11
+        CHECK(it->first == "hijklmnop"); CHECK(it->second == 2); it++;     // 11
+        CHECK(it->first == "powered"); CHECK(it->second == 2); it++;       // 11
+        CHECK(it->first == "qrs"); CHECK(it->second == 2); it++;           // 11
+        CHECK(it->first == "shell"); CHECK(it->second == 2); it++;         // 11
+        CHECK(it->first == "solar"); CHECK(it->second == 2); it++;         // 11
+        CHECK(it->first == "tuv"); CHECK(it->second == 2); it++;           // 11
+        CHECK(it->first == "wx"); CHECK(it->second == 2); it++;            // 11
+        CHECK(it->first == "yz"); CHECK(it->second == 2); it++;            // 11
+        CHECK(it->first == "0"); CHECK(it->second == 1); it++;             // 12
+        CHECK(it->first == "12"); CHECK(it->second == 1); it++;            // 12
+        CHECK(it->first == "345"); CHECK(it->second == 1); it++;           // 12
+        CHECK(it->first == "6789"); CHECK(it->second == 1); it++;          // 12
+        CHECK(it->first == "she"); CHECK(it->second == 1); it++;           // 12
+        CHECK(it->first == "solar-powered"); CHECK(it->second == 1); it++; // 12
+        CHECK(it->first == "will"); CHECK(it->second == 1); it++;          // 12
+    }
 }
 
 TEST_CASE("Checking exceptions thrown by FileWordCounter", "[exception]") {
     REQUIRE_THROWS_AS(FileWordCounter("nonExistentFile.txt"), std::runtime_error);
+}
+
+TEST_CASE("Using FileWordCounter on \'emptyTestFile.txt\'", "[empty]") {
+    FileWordCounter fwc("emptyTestFile.txt");
+    CHECK(fwc.totalWordsCounted() == 0);
+    CHECK(fwc.topNWords(10).size() == 0);
+    CHECK(fwc.count("word") == 0);
 }

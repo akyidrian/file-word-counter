282756a4743b9964f351b3a2ad013a49872a3bea
Aydin Arik <akyidrian@users.noreply.github.com>
2024-07-12 16:46:48 -0700

    General code tidy up. Added a readme and an extra test too.

--- fileWordCounter.hpp
+++ fileWordCounter.hpp
@@ -13,21 +13,21 @@ typedef std::unordered_map<std::string, FWCList::iterator> FWCMap;
 
 class FileWordCounter {
 public:
-    FileWordCounter(std::string file);
-    ~FileWordCounter();
+    FileWordCounter(const std::string file);
+    ~FileWordCounter(void);
     unsigned int count(std::string word);
-    unsigned int totalWordsCounted();
-    std::vector<FWCPair> topNWords(unsigned int n);
-    void printTopNWords(unsigned int n);
+    unsigned int totalWordsCounted(void);
+    std::vector<FWCPair> topNWords(const unsigned int n);
+    void printTopNWords(const unsigned int n);
 
 private:
-    FWCList words;
-    FWCMap nodeMap;
-    std::string file;
+    FWCList words; // sorted by count in descending order after object construction
+    FWCMap nodeMap;  // acts as a fact lookup to nodes in 'words' list
+    std::string file;  // file path
 
-    std::string cleanWord(std::string word);
-    std::queue<std::string> splitWordToken(std::string token);
-    void addWord(std::string word);
+    std::string cleanWord(const std::string word);
+    std::queue<std::string> splitWordToken(const std::string token);
+    void addWord(const std::string word);
 };
 
 #endif /* FILE_WORD_COUNTER_H */
